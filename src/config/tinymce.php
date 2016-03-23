02dd6f96bc6daa39a3c7ed6e524c219167f6cc15
Fulvio Sicurezza <f.sicurezza@gmail.com>
2016-03-23 13:30:44 +0100

    Added support for it, en_GB and zh_CN languages Extended toolbar and params configuration

--- src/config/tinymce.php
+++ src/config/tinymce.php
@@ -5,16 +5,20 @@ return [
 	'cdn' => config('app.url').'/vendor/js/tinymce/tinymce.min.js',
 
 	'params' => [
-		"selector" => "#tinymce",
-		"language" => 'pt_BR',
+		"selector" => ".tinymce",
+		"language" => 'it',
 		"theme" => "modern",
 		"skin" => "lightgray",
 		"plugins" => [
-	         "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
-	         "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-	         "save table contextmenu directionality emoticons template paste textcolor"
+			"advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+			"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+			"save table contextmenu directionality emoticons template paste textcolor"
 		],
-		"toolbar" => "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor emoticons",
+		"toolbar" => ["insertfile undo redo | paste | styleselect | sizeselect fontselect fontsizeselect bold italic | alignleft aligncenter alignright alignjustify",
+			"bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons | code"],
+		"paste_word_valid_elements" => "b,strong,i,em,h1,h2,p",
+		"paste_retain_style_properties" => "color font-size",
+		"paste_convert_word_fake_lists" => false
 	]
 
 ];
